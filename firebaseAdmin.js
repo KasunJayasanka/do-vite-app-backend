2a385ad4315ec234e9fd706ccfb10e177cc037da
Kasun Jayasanka <officialblackknightcoders@gmail.com>
2025-06-12 17:49:58 +0530

    Update firebaseAdmin.js

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -1,6 +1,6 @@
 // backend/firebaseAdmin.js
 const admin = require('firebase-admin');
-const serviceAccount = require('./firebase-service-key.json');
+const serviceAccount = require(process.env.SERVICE_ACCOUNT_KEY);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
